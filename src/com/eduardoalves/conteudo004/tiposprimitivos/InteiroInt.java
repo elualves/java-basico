8bf970c6978d60ed6a74f42e376af004c24930e9
elualves <elualvess@gmail.com>
2020-02-12 08:01:18 -0300

    Tipo primitivo int

---
+++ src/com/eduardoalves/conteudo004/tiposprimitivos/InteiroInt.java
@@ -0,0 +1,13 @@
+package com.eduardoalves.conteudo004.tiposprimitivos;
+
+public class InteiroInt {
+
+	public static void main(String[] args) {
+			//short==>>tamanho = 32 bits - intervalo de valores = -2.147.483.648 a 2.147.483.647.
+			
+			int tipoInt1 = -2147483648;
+			int tipoInt2 = 2147483647;
+			
+			System.out.println("Aceita valores no intervalo aberto entre " + tipoInt1 + " e " + tipoInt2);
+	}
+}
