d21f385c85b01970d909243cdea4b5cba853900f
elualves <elualvess@gmail.com>
2020-02-12 10:26:05 -0300

    Alçteracao

--- src/com/eduardoalves/conteudo004/tiposprimitivos/InteiroInt.java
+++ src/com/eduardoalves/conteudo004/tiposprimitivos/InteiroInt.java
@@ -3,7 +3,7 @@ package com.eduardoalves.conteudo004.tiposprimitivos;
 public class InteiroInt {
 
 	public static void main(String[] args) {
-			//short==>>tamanho = 32 bits - intervalo de valores = -2.147.483.648 a 2.147.483.647.
+			//int==>>tamanho = 32 bits - intervalo de valores = -2.147.483.648 a 2.147.483.647.
 			
 			int tipoInt1 = -2147483648;
 			int tipoInt2 = 2147483647;
