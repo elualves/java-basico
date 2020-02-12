9c827309df0fd0fbb1d88c1ed13f304ca2678fbf
elualves <elualvess@gmail.com>
2020-02-12 07:17:30 -0300

    Tipo primitivo byte

---
+++ src/com/eduardoalves/conteudo004/tiposprimitivos/InteiroByte.java
@@ -0,0 +1,14 @@
+package com.eduardoalves.conteudo004.tiposprimitivos;
+
+public class InteiroByte {
+
+	public static void main(String[] args) {
+		//byte==>>tamanho = 8 bits - intervalo de valores = -128 a 127.
+		
+		byte tipoByte1 = -128;
+		byte tipoByte2 = 127;
+		
+		System.out.println("Aceita valores no intervalo aberto de " + tipoByte1 + " e " + tipoByte2);
+	}
+
+}
