3ed413f965df6e36ba7bbf6cac382bc00b11e832
elualves <elualvess@gmail.com>
2020-03-18 18:21:20 -0300

    Multiplica os elementos de um vetor, pelos elementos de outro vetor

---
+++ src/com/eduardoalves/conteudo010/vetores/Vetor009MultiplicaElementoDeUmVetorPeloElementoDeOutroVetor.java
@@ -0,0 +1,39 @@
+package com.eduardoalves.conteudo010.vetores;
+
+import java.util.Scanner;
+
+public class Vetor009MultiplicaElementoDeUmVetorPeloElementoDeOutroVetor {
+
+		public static void main(String[] args) {
+			/*
+			 *Multiplica os elementos de um vetor pelos elementos de outro vetor, c[i]=a[i]*b[i].
+			 */
+			Scanner teclar = new Scanner(System.in);
+			int[] a = new int[5];
+			int[] b = new int[a.length];
+			int[] c = new int[a.length];
+
+			System.out.println("Informe " + a.length + " valores para o vetor a");
+			for (int i = 0; i < a.length; i++) {
+				System.out.print((i + 1) + "º valor-posição " + i + " ");
+				a[i] = teclar.nextInt();
+				System.out.print("");
+			}
+
+			System.out.println("Informe " + a.length + " valores para o vetor b");
+			for (int i = 0; i < a.length; i++) {
+				System.out.print((i + 1) + "º valor-posição " + i + " ");
+				b[i] = teclar.nextInt();
+				System.out.print("");
+				c[i] = a[i] * b[i];
+			}
+
+			System.out.println("Vetor c(vetor a + vetor b):");
+			for (int i = 0; i < a.length; i++) {
+				for (i = 0; i < a.length; i++) {
+					System.out.println("posição " + i + " valor " + c[i]);
+				}
+				teclar.close();
+			}
+		}
+	}
